0708a0ccc502862d6084e5fbbd17cb3b607232ce
John Unruh <j_unruh@hotmail.com>
2012-10-13 15:41:33 -0600

    change asset precompilation options for Heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module ACAregistration
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Configure not to use DB during precompilation
+    config.assets.initialize_on_precompile = false
   end
 end
